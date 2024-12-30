1be63f2332f5115484d6cdf691c18350bbd21529
emma_eriksen <emmag2002@live.dk>
2024-12-30 14:38:20 +0100

    favicon

--- bokse.js
+++ bokse.js
@@ -23,3 +23,29 @@ document.addEventListener("scroll", function () {
     }
   });
 });
+
+document.addEventListener("scroll", function () {
+  const fadeElements = document.querySelectorAll(".box");
+
+  fadeElements.forEach((element) => {
+    const rect = element.getBoundingClientRect();
+    const isVisible = rect.top < window.innerHeight && rect.bottom > 0;
+
+    if (isVisible) {
+      element.classList.add("visible");
+    }
+  });
+});
+
+document.addEventListener("scroll", function () {
+  const fadeElements = document.querySelectorAll(".box2");
+
+  fadeElements.forEach((element) => {
+    const rect = element.getBoundingClientRect();
+    const isVisible = rect.top < window.innerHeight && rect.bottom > 0;
+
+    if (isVisible) {
+      element.classList.add("visible");
+    }
+  });
+});
